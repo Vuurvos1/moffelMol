8ab48837e489c4598eb2041fa907e385e1515777
Firefox3000 <Firefox3000@users.noreply.github.com>
2020-03-13 17:47:03 +0100

    Initial commit
    Added images and JS and CSS and HTML. FULLSTACK DEVELOPER BABY!!! WHAT WHAT

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let images = 18;
+
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
+let day = date.getDate();
+
+let magicNumber = "" + year + month + day;
+let magicSeed = Math.floor(random(magicNumber) * images);
+
+function random(seed) {
+  var x = Math.sin(seed++) * 10000;
+  return x - Math.floor(x);
+}
+
+document.querySelector('img').src = '../img/' + magicSeed + '.jpg';
